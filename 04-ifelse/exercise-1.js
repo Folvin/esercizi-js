398ed5e5b355c32eb0f66ae8fd64da45295e56b3
Kevin Alessi <minicarota15@gmail.com>
2022-05-21 00:59:05 +0200

    fix(04): tolto (per davvero stavolta) il commento switch-case

--- 04-ifelse/exercise-1.js
+++ 04-ifelse/exercise-1.js
@@ -21,23 +21,6 @@ function calculateSalary(role) {
   }
 }
 
-function calculateSalary(role) {
-  switch (role) {
-    case `ceo`:
-      return 2200;
-
-    case `manager`:
-    case `cto`:
-      return 1800;
-
-    case `developer`:
-      return 1500;
-
-    default:
-      return 1000;
-  }
-}
-
 
 const ceoSalary = calculateSalary('ceo');
 const managerSalary = calculateSalary('manager');
