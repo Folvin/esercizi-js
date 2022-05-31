9bf29c11842f7019ece73e78cef1f19b7f22e3f0
Kevin Alessi <minicarota15@gmail.com>
2022-05-31 22:15:03 +0200

    chore(17): aggiunto un commento

--- 17-Closures-e-concatenazione/exercise-4.js
+++ 17-Closures-e-concatenazione/exercise-4.js
@@ -25,3 +25,5 @@ function calculate() {
 }
 const calculator = calculate();
 calculator.add(2).add(4).multiply(3).sub(1).sub(3).divide(2).printResult(); // Il risultato sarà: 7
+
+/* è stato un parto logico lungo 15 minuti */
