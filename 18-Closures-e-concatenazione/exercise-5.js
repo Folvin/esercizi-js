a6ffbaa37b280c0cec752fefe1abd07cb5bee645
Kevin Alessi <minicarota15@gmail.com>
2022-06-15 19:07:54 +0200

    fix(18): corretto il funzionamento della cache

--- 18-Closures-e-concatenazione/exercise-5.js
+++ 18-Closures-e-concatenazione/exercise-5.js
@@ -3,32 +3,26 @@ function memoize(fn) {
 
   return function (x) {
     if (x in cache) {
-      return `fetching result from cache ${cache[x]}`
+      console.log(`fetching from cache ${x}! that is equal to ${cache[x]}`)
+      return cache[x]
     }
-    cache[x] = fn(x)
 
-    return `calculating result from function ${fn(x)}`
+    return cache[x] = fn(x)
   }
 }
-
 function factorial(x) {
   if (x === 0) {
     return 1;
   }
-
+  console.log(`calculating ${x} * ${x - 1}!`)
   return x * factorial(x - 1);
 }
 
-
-factorization = memoize(factorial);
-console.log(factorization(10));
-console.log(factorization(4));
-console.log(factorization(10));
-console.log(factorization(13));
-console.log(factorization(7));
-console.log(factorization(4));
-console.log(factorization(0));
-console.log(factorization(10));
-
-
-
+factorial = memoize(factorial);
+console.log(factorial(5));
+console.log(factorial(7));
+console.log(factorial(2));
+console.log(factorial(14));
+console.log(factorial(16));
+console.log(factorial(0));
+console.log(factorial(20));
