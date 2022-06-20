905fe753ac39ff76fc84385383d448a86cb6b557
Kevin Alessi <minicarota15@gmail.com>
2022-06-20 10:15:40 +0200

    style: abbellito il console log

--- 18-Closures-e-concatenazione/exercise-5.js
+++ 18-Closures-e-concatenazione/exercise-5.js
@@ -3,7 +3,8 @@ function memoize(fn) {
 
   return function (x) {
     if (x in cache) {
-      console.log(`fetching from cache ${x}! that is equal to ${cache[x]}`)
+      console.log(`fetching from cache ${x}! that is equal to ${cache[x]}
+the result of the function is`)
       return cache[x]
     }
 
@@ -12,9 +13,11 @@ function memoize(fn) {
 }
 function factorial(x) {
   if (x === 0) {
+    console.log(`calculating 0! = 1
+the result of the function is`)
     return 1;
   }
-  console.log(`calculating ${x} * ${x - 1}!`)
+  console.log(`calculating ${x}! that is ${x} * ${x - 1}!`)
   return x * factorial(x - 1);
 }
 
