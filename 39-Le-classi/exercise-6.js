4a8a8ae1d1d11651916f81681714e66b60a54a08
Kevin Alessi <minicarota15@gmail.com>
2022-06-22 04:06:24 +0200

    feat: esercizi 38~40 completati

---
+++ 39-Le-classi/exercise-6.js
@@ -0,0 +1,32 @@
+class Square {
+  constructor(side) {
+    this.side = side;
+  }
+}
+
+class Rectangle {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+}
+
+class Circle {
+  constructor(radius) {
+    this.radius = radius;
+  }
+}
+
+class AreaCalculator {
+  static calculate(polygon) {
+    return polygon.side**2 || polygon.width * polygon.height || polygon.radius**2*3.14
+  }
+}
+
+const square = new Square(4);
+const rectangle = new Rectangle(4, 2);
+const circle = new Circle(5);
+
+console.log(AreaCalculator.calculate(square));
+console.log(AreaCalculator.calculate(rectangle));
+console.log(AreaCalculator.calculate(circle));
